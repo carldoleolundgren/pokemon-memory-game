b7619e97d397917f0ff2196a586fc1eb79b3095a
Carl D'Oleo-Lundgren <carl.doleo.lundgren@gmail.com>
2021-08-03 16:50:19 -0400

    now all API data is cached in localStorage for performance improvements

--- src/App.js
+++ src/App.js
@@ -2,53 +2,61 @@ import { useEffect, useState } from 'react';
 import PokeCards from './components/PokeCards'
 
 function App() {
-  const [pokemon, setPokemon] = useState([])
+  const [currentPokemon, setCurrentPokemon] = useState([])
   const [isLoading, setLoading] = useState(true)
   
   function getRandomPokeIndex() {
     return Math.floor(Math.random() * (898 - 1) + 1);
   }
 
-  async function getPokemon(numOfPokemon) {
-    let pokemonCopy = [...pokemon]
+  
+  async function getAllPokemon() {
+    let pokemonArr = []
+    let numOfRounds = 4;
+    let numOfPokemonPerRound = [5, 10, 15, 20]
     setLoading(true)
     
-    for (let i = 0; i < numOfPokemon; i++) { 
-      let newPokemon = {}
-      let randomPokeIndex = getRandomPokeIndex()
-      const pokeData = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${randomPokeIndex}/`, {mode: 'cors'})
-      const pokeDataJson = await pokeData.json()
-      
-      newPokemon.name = pokeDataJson.name
-      newPokemon.src = pokeDataJson.sprites.front_default
-      
-      pokemonCopy.push(newPokemon)
-      
-      console.log(pokemonCopy)
+    for (let i = 0; i < numOfRounds; i++) {
+      let gameRoundArr = []
+      for (let j = 0; j < numOfPokemonPerRound[i]; j++) {
+        let newPokemon = {}
+        let pokeData = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${getRandomPokeIndex()}/`, {mode: 'cors'})
+        let pokeDataJSON = await pokeData.json()
+
+        newPokemon.name = pokeDataJSON.name
+        newPokemon.src = pokeDataJSON.sprites.front_default
+
+        gameRoundArr.push(newPokemon)
+      }
+      pokemonArr.push(gameRoundArr)
     }
     
-    setPokemon(pokemonCopy)
+    localStorage.setItem('pokemonStorage', JSON.stringify(pokemonArr))
+    getCurrentPokemon(0)
     setLoading(false)
+  }
+  
+  function getCurrentPokemon(nthRound) {
+    let currentPokemon = JSON.parse(localStorage.getItem('pokemonStorage'))
+    setCurrentPokemon(currentPokemon[nthRound])
   }
 
   useEffect(() => {
-    getPokemon(5)
+    getAllPokemon() 
+
     // eslint-disable-next-line 
   }, [])
   
   return (
     <div>
-      {isLoading ? <div>Loading...</div> 
-      : 
-        <PokeCards
-          pokemon={pokemon}
-        />
-    }
+      {isLoading 
+        ? <div>Loading your randomized Pokédex...</div> 
+        : <PokeCards
+            currentPokemon={currentPokemon}
+          />
+      }
     </div>
   );
 }
 
 export default App;
-
-//make each PokeCard a Material-UI card
-
--- src/components/PokeCards.js
+++ src/components/PokeCards.js
@@ -1,18 +1,40 @@
 /* import React, { useState } from 'react' */
 import uniqid from 'uniqid' 
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardHeader from '@material-ui/core/CardHeader'
+
+const styles = {
+  pokeCardContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  pokeCard: {
+    width: '163px',
+    textAlign: 'center',
+    position: 'relative',
+    float: 'left'
+  }
+}
 
 const PokeCard = (props) => {
   return (
-    <div>
-      <img src={props.src} alt={props.name}></img>
-      {props.name}
-    </div>
+    <Card
+      style={styles.pokeCard}
+    >
+      <CardActionArea
+        onClick={() => console.log(props.name)}
+      >
+        <CardHeader title={props.name}/>
+        <img src={props.src} alt={props.name}></img>
+      </CardActionArea>
+    </Card>
   )
 }
 
 const PokeCards = (props) => {
-  const pokemon = props.pokemon
-  const pokeCardList = pokemon.map((p) => (
+  const currentPokemon = props.currentPokemon
+  const pokeCardList = currentPokemon.map((p) => (
     <PokeCard 
       key={uniqid()}
       src={p.src}
@@ -21,7 +43,9 @@ const PokeCards = (props) => {
   ))
   
   return (
-    <div>
+    <div
+      style={styles.pokeCard}
+    >
       {pokeCardList}
     </div>
   )
